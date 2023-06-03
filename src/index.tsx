f6e2020e7b6dacd85cf7de6a340384a1d1716a80
CyrilStrone <cyrilstrone@gmail.com>
2023-06-04 03:55:40 +0700

    StrictMode and HomePage

--- src/index.tsx
+++ src/index.tsx
@@ -9,9 +9,11 @@ const root = ReactDOM.createRoot(
     document.getElementById('root') as HTMLElement
 );
 root.render(
-    <HashRouter >
-        <App />
-    </HashRouter>
+    <React.StrictMode>
+        <HashRouter >
+            <App />
+        </HashRouter>
+    </React.StrictMode>
 );
 
 reportWebVitals();
